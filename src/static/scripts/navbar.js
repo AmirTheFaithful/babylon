c49a0bb4557d95ac32413f40937230e3b552fd66
Amir Daneshmand <amirdanesh02@gmail.com>
2024-08-06 21:42:58 +0330

    Complete dropdown menu

--- src/static/scripts/navbar.js
+++ src/static/scripts/navbar.js
@@ -1,9 +1,23 @@
 function toggleDropdown() {
   let dropdown = document.getElementById("dropdown-menu");
 
+  // Open dropdown itself
   if (dropdown.className == "dropdown-disabled") {
     dropdown.className = "dropdown-enabled";
   } else if (dropdown.className == "dropdown-enabled") {
     dropdown.className = "dropdown-disabled";
+  }
+
+  // Toggle button's image
+  let icon = document.getElementById("hamburger-icon");
+
+  if (icon.classList.contains("menuIcon")) {
+    icon.classList.remove("menuIcon")
+    icon.classList.add("closeIcon")
+    icon.innerHTML = "close";
+  } else {
+    icon.classList.remove("closeIcon");
+    icon.classList.add("menuIcon")
+    icon.innerHTML = "menu";
   }
 }
