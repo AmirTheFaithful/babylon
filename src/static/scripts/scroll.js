f3552ed7d261817c008bcd041989292ff29ce3ef
Amir Daneshmand <amirdanesh02@gmail.com>
2024-08-11 16:47:03 +0330

    Minor improvements of home page and code clealiness

--- src/static/scripts/scroll.js
+++
@@ -1,8 +0,0 @@
-function renderCard(direction="row", point="top", data, color="#a9a0ff70") {
-  let card = document.createElement("section");
-  card.className = "card";
-  card.style.flexDirection = direction;
-
-  let main = document.getElementById("main")
-  main.append(card);
-}
