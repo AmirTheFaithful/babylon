0131dd9515dc7f44c9420a63ece199b8c6eff04c
Amir Daneshmand <amirdanesh02@gmail.com>
2024-08-26 02:08:21 +0330

    Add cookie management

---
+++ src/static/scripts/cookies.js
@@ -0,0 +1,25 @@
+const cookiesBox = document.querySelector("#cookies-box");
+const consentBtn = document.querySelector(".consent-btn");
+const rejectBtn = document.querySelector(".reject-btn");
+
+const hideBox = () => {
+  cookiesBox.classList.add("hide");
+};
+
+consentBtn.onclick = function () {
+  if (document.cookie) {
+    hideBox();
+  } else {
+    document.cookie = `CookieBy=Babylon; max-age=${60 * 60 * 24}`;
+    hideBox();
+  }
+};
+
+rejectBtn.onclick = function () {
+  document.cookie = "CookieBy=Babylon; rejected=true";
+  hideBox();
+};
+
+let checkCookie = document.cookie.indexOf("CookieBy=Babylon");
+
+checkCookie !== -1 ? hideBox() : cookiesBox.classList.remove("hide");
