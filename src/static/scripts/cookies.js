9850e9d5cd62d142ef88b3d32a6ac4a2488f5460
Amir Daneshmand <amirdanesh02@gmail.com>
2024-08-30 17:23:01 +0330

    Add favicon.ico

--- src/static/scripts/cookies.js
+++ src/static/scripts/cookies.js
@@ -10,13 +10,13 @@ consentBtn.onclick = function () {
   if (document.cookie) {
     hideBox();
   } else {
-    document.cookie = `CookieBy=Babylon; max-age=${60 * 60 * 24}`;
+    document.cookie = `CookieBy=Babylon, max-age=${60 * 60 * 24};`;
     hideBox();
   }
 };
 
 rejectBtn.onclick = function () {
-  document.cookie = "CookieBy=Babylon; rejected=true";
+  document.cookie = "CookieBy=Babylon, rejected=true;";
   hideBox();
 };
 
@@ -24,6 +24,6 @@ let checkCookie = document.cookie.indexOf("CookieBy=Babylon");
 
 checkCookie !== -1 ? hideBox() : cookiesBox.classList.remove("hide");
 
-// window.addEventListener("load", () => {
-//   cookiesBox.classList.remove("hide");
-// });
+window.addEventListener("load", () => {
+  cookiesBox.classList.remove("hide");
+});
